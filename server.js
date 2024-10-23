85a879482f7d1457783d84ea6b42750139aa19fd
yousuf-4594 <cysiddiqui@gmail.com>
2024-10-23 23:43:10 +0500

    commit message

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const port = process.env.PORT || 8080;
+
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(process.env.PORT || 8080, (err) => {
-    if (err) throw err;
-    console.log("> Ready on http://localhost:8080");
+  }).listen(port, () => {
+    console.log(`> Ready on port ${port}`);
   });
 });
